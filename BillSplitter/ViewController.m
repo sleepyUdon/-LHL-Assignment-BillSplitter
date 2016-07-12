7c134939625f5ad0cc47aef2864d734678bc3369
sleepyUdon <vivianechan@hotmail.com>
2016-07-12 19:02:46 -0400

    file updated

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -15,6 +15,7 @@
 
 @property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
 
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeople;
 
 @property (weak, nonatomic) IBOutlet UILabel *amountToPayLabel;
 
@@ -24,15 +25,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupBillAmountField];
+    [self customizeKeyboard];
+    [self.numberOfPeopleSlider addTarget:self action:@selector(outputSlider:) forControlEvents:UIControlEventValueChanged];
+
+    [self outputSlider:self.numberOfPeopleSlider];
+
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma set up
+
+-(void) setupBillAmountField{
+    self.totalBillAmountTextField.placeholder = @"Type bill amount";
+}
+
+
+
+-(void) setSlider {
+    self.numberOfPeopleSlider.minimumValue = 1;
+    self.numberOfPeopleSlider.maximumValue = 7;
+    self.numberOfPeopleSlider.value = 5;
+}
+
+
+#pragma action
 
 - (IBAction)calculateSplitAmount:(id)sender {
     NSDecimalNumber * amountToPay= [NSDecimalNumber decimalNumberWithString: _totalBillAmountTextField.text];
@@ -41,7 +63,33 @@
     NSDecimalNumber *contribution = [amountToPay decimalNumberByDividingBy:divisor];
 
     _amountToPayLabel.text = contribution.stringValue;
+}
 
+
+
+-(void) customizeKeyboard{
+    self.totalBillAmountTextField.keyboardType = UIKeyboardTypeDecimalPad;
+}
+
+- (void)outputSlider:(UISlider *)sender {
+    NSNumberFormatter * formatter = [[NSNumberFormatter alloc] init];
+    [formatter setMaximumFractionDigits:0];
+
+    
+    self.numberOfPeople.text = [NSString stringWithFormat:@"%@", [formatter stringFromNumber: [NSNumber numberWithFloat:self.numberOfPeopleSlider.value]]];
+}
+
+#pragma mark - Handle Gestures
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if ([self.totalBillAmountTextField isFirstResponder]) {
+        [self.totalBillAmountTextField resignFirstResponder];
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)totalBillAmountTextField {
+    [totalBillAmountTextField resignFirstResponder];
+    return YES;
 }
 
 @end
